8067c486234349e64e157b8d05077f0d558e0044
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-09-29 18:41:28 +0100

    Fetching jobs data from mock server API

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,26 @@
 import PropTypes from 'prop-types';
-import jobs from '../jobs.json';
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false }) => {
-  const jobsListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching jobs data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -12,7 +29,7 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobsListings.map((job) => {
+          {jobs.map((job) => {
             return <JobListing key={job.id} job={job}></JobListing>;
           })}
         </div>
