a45b34dda136852c20de20c8254f5c3b7d1a4dbe
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-09-29 18:33:45 +0100

    Mock server added using json-server

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
       <Hero></Hero>
       <HomeCards></HomeCards>
-      <JobListings></JobListings>
+      <JobListings isHome={true}></JobListings>
       <ViewAllJobs></ViewAllJobs>
     </>
   );
